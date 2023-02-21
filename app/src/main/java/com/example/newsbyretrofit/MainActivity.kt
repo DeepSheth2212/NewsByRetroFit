a6cb297640f1e102f096c73b87ebb6e0dc009a9b
Deep Sheth <shethdeep22@gmail.com>
2023-02-21 22:03:21 +0530

    second commit -UI related changes(Material3 theme added) -SpinKit progressbar added!

--- app/src/main/java/com/example/newsbyretrofit/MainActivity.kt
+++ app/src/main/java/com/example/newsbyretrofit/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.newsbyretrofit
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.github.ybq.android.spinkit.style.Wave
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -13,9 +16,14 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity() {
     lateinit var adapter: NewsAdapter
     private var articles  = mutableListOf<Article>()
+    lateinit var progressBar: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        progressBar = findViewById<ProgressBar>(R.id.spin_kit)
+        val wave = Wave()
+        progressBar.setIndeterminateDrawable(wave);
         adapter = NewsAdapter(this@MainActivity, articles)
         newsList.adapter = adapter
 
@@ -30,16 +38,19 @@ class MainActivity : AppCompatActivity() {
         news.enqueue(object : Callback<News> {
 
             override fun onFailure(call: Call<News>, t: Throwable) {
+                progressBar.visibility= View.GONE
                 Log.d("tag", "Error! ", t)
             }
 
             override fun onResponse(call: Call<News>, response: Response<News>) {
+
                 val news = response.body()
 
                 if (news != null) {
                     //Log.d("tag", news.toString())
                     articles.addAll(news.articles)
                     adapter.notifyDataSetChanged()
+                    progressBar.visibility= View.GONE
                 }
             }
 
